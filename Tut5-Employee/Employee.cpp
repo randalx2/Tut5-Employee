77c95759c25aa1291a287598bc89a4410d2bc804
randalx2 <pmisthry@gmail.com>
2016-03-18 23:14:47 +0200

    Completed Class Definitions

--- Tut5-Employee/Employee.cpp
+++ Tut5-Employee/Employee.cpp
@@ -7,3 +7,206 @@
 #include "Employee.h"
 
 using namespace std;
+
+//First Initialize the static variable OUTSIDE THE CLASS DECLARATION
+
+int Employee::numberOfEmployees = 0;   //DON'T SPECIFY THE STATIC KEYWORD HERE
+                                      //ONLY USE STATIC IN THE DECLARATION. Static variables are only
+                                     //initialized once.
+                                   //Can access static variable of class scope outside class without creating any object
+Employee::Employee(){
+	
+	nameOfEmployee = "Any Employee";
+	employeeNumber = 0;
+	numberOfEmployees++;   //Whenever a new object is created a new employee is created
+	salaryOfEmployee = 0.00;
+}
+
+Employee::Employee(string iname, int inumber){
+
+	nameOfEmployee = iname;
+	if (inumber >= 0)
+	{
+		employeeNumber = inumber;
+	}
+	else
+	{
+		cout << "Invalid Staff Number Input. Try again" << endl;
+		cin >> employeeNumber;
+	}
+	numberOfEmployees++;
+	salaryOfEmployee = 0.00;
+}
+
+Employee::~Employee(){
+	numberOfEmployees--; //Employee lost when an object is destroyed
+}
+
+//No Need to develop bodies for pure virtual functions in abstract base class
+//Instead define their overriding functions in the sub classes
+
+SalaryEmployee::SalaryEmployee(){
+
+	nameOfEmployee = "Monthly";
+	employeeNumber = 0;
+	numberOfEmployees++;
+	salaryOfEmployee = 0.00;
+}
+
+SalaryEmployee::SalaryEmployee(string iname, int inumber)
+{
+	nameOfEmployee = iname;
+
+	if (inumber >= 0){
+		employeeNumber = inumber;
+	}
+	else
+	{
+		cout << "Invalid Staff Number input. Ensure a positive number now" << endl;
+		cin >> employeeNumber;
+	}
+	
+	numberOfEmployees++;
+	salaryOfEmployee = 0.00;
+}
+
+SalaryEmployee::~SalaryEmployee(){
+	numberOfEmployees--;
+}
+
+float SalaryEmployee::setSalary(const float iSalary){
+	monthlySalary = iSalary;
+	return monthlySalary;
+}
+
+float SalaryEmployee::salary(){
+	return monthlySalary;
+}
+
+string SalaryEmployee::name(){
+	return nameOfEmployee;
+}
+
+int SalaryEmployee::staffNumber(){
+	return employeeNumber;
+}
+
+HourlyEmployee::HourlyEmployee(){
+	nameOfEmployee = "Hourly";
+	employeeNumber = 0;
+	numberOfEmployees++;
+	salaryOfEmployee = 0.00;
+}
+
+HourlyEmployee::HourlyEmployee(string iname, int inumber)
+{
+	nameOfEmployee = iname;
+
+	if (inumber >= 0){
+		employeeNumber = inumber;
+	}
+	else
+	{
+		cout << "Invalid Staff Number input. Ensure a positive number now" << endl;
+		cin >> employeeNumber;
+	}
+
+	numberOfEmployees++;
+	salaryOfEmployee = 0.00;
+}
+
+HourlyEmployee::~HourlyEmployee()
+{
+	numberOfEmployees--;
+}
+
+float HourlyEmployee::setHourlyRate(float ihourlySalary)
+{
+	hourlySalary = ihourlySalary;
+	return hourlySalary;
+}
+
+int HourlyEmployee::setHoursWorked(int ihours)
+{
+	hoursWorked = ihours;
+	return hoursWorked;
+}
+
+float HourlyEmployee::salary()
+{
+	salaryOfEmployee = hourlySalary*hoursWorked;
+	return salaryOfEmployee;
+}
+
+string HourlyEmployee::name()
+{
+	return nameOfEmployee;
+}
+
+int HourlyEmployee::staffNumber()
+{
+	return employeeNumber;
+}
+
+CommissionEmployee::CommissionEmployee()
+{
+	nameOfEmployee = "Commission";
+	employeeNumber = 0;
+	numberOfEmployees++;
+	salaryOfEmployee = 0.00;
+}
+
+CommissionEmployee::CommissionEmployee(string iname, int inumber)
+{
+	nameOfEmployee = iname;
+
+	if (inumber >= 0){
+		employeeNumber = inumber;
+	}
+	else
+	{
+		cout << "Invalid Staff Number input. Ensure a positive number now" << endl;
+		cin >> employeeNumber;
+	}
+
+	numberOfEmployees++;
+	salaryOfEmployee = 0.00;
+}
+
+CommissionEmployee::~CommissionEmployee(){
+	numberOfEmployees--;
+}
+
+float CommissionEmployee::setBaseSalary(float ibase)
+{
+	baseSalary = ibase;
+	return baseSalary;
+}
+
+float CommissionEmployee::setRate(float irate)
+{
+	commRate = irate;
+	return commRate;
+}
+
+float CommissionEmployee::setRevenue(float irev)
+{
+	revGenerated = irev;   //NB Send a decimal percentage value
+	return revGenerated;
+}
+
+float CommissionEmployee::salary()
+{
+	salaryOfEmployee = baseSalary + commRate*revGenerated;
+	return salaryOfEmployee;
+}
+
+string CommissionEmployee::name()
+{
+	return nameOfEmployee;
+}
+
+int CommissionEmployee::staffNumber()
+{
+	return employeeNumber;
+}
--- Tut5-Employee/Employee.h
+++ Tut5-Employee/Employee.h
@@ -7,30 +7,33 @@ using namespace std;
 
 class Employee {
 protected: static int numberOfEmployees;  //Only one copy available to keep track of number of employees
+
 		   string nameOfEmployee;  //Employee name -- to be set by constructor
-		   int salary;
+		   float salaryOfEmployee;   //Salary of employee by month end
 		   int employeeNumber;  //Number assinged to each employee -- to be set by constructor
 
 public: Employee();
 		Employee(string, int);
+		~Employee();
 
+		//Pure Virtual Functions for Abstract Class
 	    virtual string name() = 0;
-		virtual int staffNumber() = 0;
-		virtual float salary() = 0;
+		virtual int staffNumber() = 0;  //Returns staff number of employee --POSITIVE INTEGER
+		virtual float salary() = 0;    //Returns amount of money PAID AT MONTH END
 };
 
 class SalaryEmployee : public Employee{
-private: float monthlySalary;
+private: float monthlySalary; //FIXED MONTHLY SALARY
 public:
 	SalaryEmployee();
 	SalaryEmployee(string, int);
 	~SalaryEmployee();
 
 	//Methods for this subclass
-	float setSalary();
+	float setSalary(const float);  //Set employees FIXED monthly salary -- can also be used to override Abstract class value
 
 	//Overriding Functions
-	float salary();
+	float salary();    //Overide the monthly salary based on employees unique profile
 	string name();
 	int staffNumber();
 };
@@ -44,31 +47,31 @@ public: HourlyEmployee();
 		~HourlyEmployee();
 
 		//Methods for this sub class
-	    float setHourlyRate();
-		int setHoursWorked();
+	    float setHourlyRate(float);  //Returns hourly rate
+		int setHoursWorked(int);   //Returns hours worked
 
 		//Overriding functions
-		float salary();
+		float salary();       //Overide the monthly salary based on employees unique profile
 		string name();
 		int staffNumber();	
 };
 
 class CommissionEmployee : public Employee{
-private: float baseSalary;
-		 float commRate;
-		 float revGenerated;
+private: float baseSalary;   
+		 float commRate;     
+		 float revGenerated;  
 
 public: CommissionEmployee();
 		CommissionEmployee(string, int);
 		~CommissionEmployee();
 
 		//Methods for this sub class
-		float setBaseSalary();
-		float setRate();
-		float setRevenue;
+		float setBaseSalary(float);  //Set the basic
+		float setRate(float);        // Set the commission
+		float setRevenue(float);       // Set revenue
 
 		//Overriding functions
-		float salary();
+		float salary();       //Overide the monthly salary based on employees unique profile
 		string name();
 		int staffNumber();
 };
